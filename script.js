b76187b0d5ec775a1b6e7514d1d84a8b34359f89
Bonnie Reed <bonniejeanreed@gmail.com>
2023-03-21 11:53:57 -0500

    clean slate, again. :)

---
+++ script.js
@@ -0,0 +1,17 @@
+let currentListing = document.getElementById("current-listings");
+let createListing = document.getElementById("create-listings");
+let directMessages = document.getElementById("direct-messages");
+
+switch (newTab) {
+    case currentListing:
+        newTab = `https://www.facebook.com/marketplace/you/selling`;
+        break;
+    case createListing:
+        newTab = `https://www.facebook.com/marketplace/you/selling`;
+        break;
+    case directMessages:
+        newTab = `https://www.facebook.com/marketplace/you/selling`;
+        break;
+    default:
+        break;
+}
